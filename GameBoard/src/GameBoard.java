0ce5d5a6c672924ebbc8b8d20afcb01486b1de8c
Sheree <sheree.edmund@gmail.com>
2021-12-10 12:07:09 -0500

    fourth commit

--- GameBoard/src/GameBoard.java
+++ GameBoard/src/GameBoard.java
@@ -3,45 +3,80 @@ import java.util.Scanner;
 
 public class GameBoard {
     public static void main(String[] args) {
+        //Get standard input with Java using a Scanner, or use Processing to get mouse, keyboard, or other input.
         Scanner in = new Scanner(System.in);
+        // score keeper
+        int wins = 0;
+        int loses = 0;
+        int ties = 0;
+
+
+
+
         // while loop to allow user to continue playing. For loop isn't used since we don't know
-        // amount of moves
+
 
         while(true) {
+
+        System.out.println("Welcome to Rock, Paper, Scissors!");
+        System.out.println("MAIN MENU");
+        System.out.println("1. Type 'play' to play");
+        System.out.println("2. Type 'history' to view your game history.");
+        System.out.println("3. Type 'quit' to stop playing.");
         System.out.println("Please begin the game by entering your first move. choices are rock, paper, or scissors. If you would like to quit, please enter quit.");
-        String playerMove = in.nextLine();
+        //Handle incorrect capitalization of otherwise valid user input ("rock," "Rock," "RoCk," "ROCK," and more).
+        String playerMove = in.nextLine().toLowerCase();
+
             if(playerMove.equals("quit")) {
                 break;
             }
 
             // validation of moves
+//            if(playerMove.equals("play")) {
+//
+//            }
             if (!playerMove.equals("rock") && !playerMove.equals("paper") && !playerMove.equals("scissors")) {
+              //Handle invalid user input.
                 System.out.println("This is not a valid move, please chose: rock, paper or scissors");
             } else {
                 //code to randomize the computer move this will generate a
                 // number between 0 and 2. (int) converts it from a decimal.
                 int randomMove = (int) (Math.random() * 3);
                 // set it to an empty String
-                String player2Move = "";
+                String computerMove = "";
                 if (randomMove == 0) {
-                    player2Move = "rock";
+                    computerMove = "rock";
                 } else if (randomMove == 1) {
-                    player2Move = "paper";
+                    computerMove = "paper";
                 } else {
-                    player2Move = "scissors";
+                    computerMove = "scissors";
                 }
                 // string concat
-                System.out.println("Player 2 move: " + player2Move);
+                System.out.println("Computer move: " + computerMove);
 
                 // check winning conditions
-                if (playerMove.equals(player2Move)) {
+                if (playerMove.equals(computerMove)) {
                     System.out.println("It is a tie");
-                } else if ((playerMove.equals("rock") && player2Move.equals("scissors")) || (playerMove.equals("paper") && player2Move.equals("rock")) || (playerMove.equals("scissors") && player2Move.equals("paper"))) {
+                    ties++;
+                } else if ((playerMove.equals("rock") && computerMove.equals("scissors")) || (playerMove.equals("paper") && computerMove.equals("rock")) || (playerMove.equals("scissors") && computerMove.equals("paper"))) {
                     System.out.println("Winner winner chicken dinner!");
+                    wins++;
                 } else {
                     System.out.println("Keep your chin up Chuck, you'll get them next time!");
+                    loses++;
                 }
             }
+            // score printout
+            System.out.println("Wins: " + wins);
+            System.out.println("Loses: " + loses);
+            System.out.println("Ties: " + ties);
+        }
+        if(wins > loses ) {
+            System.out.println("you won " + wins + " games and loss " + loses + " games");
+        }else if(wins < loses) {
+            System.out.println("you loss " + loses + " games and won " + wins);
+        } else {
+            System.out.println("you have " + ties + "tie games");
         }
         System.out.println("Player selected quit, please click run when you are ready to play again.");
     }
